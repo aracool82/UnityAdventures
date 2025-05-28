22494db7b0f19975db69bb786742f6931201eefa
aracool82 <aracool1982@gmail.com>
2025-05-28 17:43:59 +0700

    add Condision

--- _Project20-21/SailingShip/Scripts/Ship.cs
+++ _Project20-21/SailingShip/Scripts/Ship.cs
@@ -6,29 +6,31 @@ namespace _Project20_21.SailingShip.Scripts
     public class Ship : MonoBehaviour
     {
         [SerializeField] private Wind _wind;
-        //[SerializeField] private float _speed;
+        [SerializeField] private Transform _target;
 
         private const float RorationSpeed = 20f;
 
         private Rigidbody _rigidbody;
-        private Rotator _rotator;
+       // private Rotator _rotator;
         private Vector3 _force;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
-            _rotator = new Rotator(transform,RorationSpeed,KeyCode.W,KeyCode.Q);
-            if (_rotator == null)
-            {
-                Debug.LogError("No Rotator attached");
-                Application.Quit();
-            }
+            // _rotator = new Rotator(transform,RorationSpeed,KeyCode.W,KeyCode.Q);
+            // if (_rotator == null)
+            // {
+            //     Debug.LogError("No Rotator attached");
+            //     Application.Quit();
+            // }
 
         }
 
+        public Vector3 ParusDirection => _target.forward;
+        
         private void Update()
         {
-            _rotator.Update();
+            //_rotator.Update();
 
             //_force = _wind.Direction * _speed * Time.deltaTime;
         }
--- _Project20-21/SailingShip/Scripts/Wind.cs
+++ _Project20-21/SailingShip/Scripts/Wind.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _Project20_21.SailingShip.Scripts
 {
@@ -9,19 +11,51 @@ namespace _Project20_21.SailingShip.Scripts
         [SerializeField] private float _changeDirectionTime;
         private float _time;
 
-        public Vector3 Direction => transform.forward;
+        private AngleLess90Condition _angleLess90Condition;
+        private float _minAngle = 0;
+        private float _maxAngle = 90;
+        [SerializeField ,Range(0,1)]private  float _power;
+
+        private void Awake()
+        {
+            _angleLess90Condition = new AngleLess90Condition(transform);
+        }
 
         private void Update()
         {
-            transform.position = new Vector3(_ship.transform.position.x, transform.position.y, _ship.transform.position.z);
-            
+            float _currentAngle = Vector3.Angle(transform.forward, _ship.ParusDirection);
+
+            if (_currentAngle < _maxAngle && _currentAngle > _minAngle)
+                if (_currentAngle == 0)
+                    _power = 1;
+                else
+                    _power = _minAngle / _currentAngle;
+            else
+                _power = 0;
+
+            //transform.position =
+            //    new Vector3(_ship.transform.position.x, transform.position.y, _ship.transform.position.z);
+
             _time += Time.deltaTime;
 
             if (_time >= _changeDirectionTime)
             {
                 _time = 0;
-                transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0f, 180), 0));
+                //transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0f, 180), 0));
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            _angleLess90Condition = new AngleLess90Condition(transform);
+            Vector3 start = new Vector3(0, 1, -1);
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(start, transform.forward);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(start, _ship.ParusDirection);
+            float angle = Vector3.Angle(transform.forward, _ship.ParusDirection);
+           // Debug.Log(angle);
         }
     }
 }
