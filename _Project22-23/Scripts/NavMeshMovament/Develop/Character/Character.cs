2b22fec944ad21fa2ff25af81d099bf0701f42b5
aracool82 <aracool1982@gmail.com>
2025-07-13 03:08:46 +0700

    modifed characterView and Character

--- _Project22-23/Scripts/NavMeshMovament/Develop/Character/Character.cs
+++ _Project22-23/Scripts/NavMeshMovament/Develop/Character/Character.cs
@@ -2,36 +2,53 @@ using UnityEngine;
 
 namespace _Project22_23.Scripts.NewNavMeshScripts
 {
-    public class Character : MonoBehaviour,IDirectionalMovable,IDirectionalRotatable
+    public class Character : MonoBehaviour, IDirectionalMovable, IDirectionalRotatable, IDamageble
     {
         [SerializeField] private float _moveSpeed = 5f;
         [SerializeField] private float _rotationSpeed = 900f;
+        [SerializeField] private CharacterView _view;
+        [SerializeField] private float _maxHealth = 120;
 
         private DirectionRotator _rotator;
         private DirectionMover _mover;
+        private Health _health;
 
+        public float Health => _health.Value;
+        public float MaxHealth => _health.MaxHealth;
         public bool IsMoved => _mover.CurrentVelocity != Vector3.zero;
+        public bool IsAlive => _health.IsAlive;
         public Vector3 CurrentVelocity => _mover.CurrentVelocity;
         public Quaternion CurrentRotation => _rotator.CurrentRotation;
         public Transform Transform => transform;
-        
+
         private void Awake()
         {
+            _health = new Health(_maxHealth);
             _mover = new DirectionMover(GetComponent<CharacterController>(), _moveSpeed);
             _rotator = new DirectionRotator(transform, _rotationSpeed);
         }
 
         private void Update()
         {
-            _mover.Update(Time.deltaTime);
-            _rotator.Update(Time.deltaTime);
+            if (IsAlive)
+            {
+                _mover.Update(Time.deltaTime);
+                _rotator.Update(Time.deltaTime);
+            }
         }
 
         public void SetMoveDirection(Vector3 direction)
-            =>_mover.SetDirection(direction);
+            => _mover.SetDirection(direction);
 
 
         public void SetRotationDirection(Vector3 direction)
-            =>_rotator.SetDirection(direction);
+            => _rotator.SetDirection(direction);
+
+
+        public void TakeDamage(float amount)
+        {
+            _health.TakeDamage(amount);
+            _view.SetAnimationTakeDamage();
+        }
     }
 }
--- _Project22-23/Scripts/NavMeshMovament/Develop/Character/CharacterView.cs
+++ _Project22-23/Scripts/NavMeshMovament/Develop/Character/CharacterView.cs
@@ -1,26 +1,59 @@
+using TMPro;
 using UnityEngine;
 
 namespace _Project22_23.Scripts.NewNavMeshScripts
 {
-    public class CharacterView: MonoBehaviour
+    public class CharacterView : MonoBehaviour
     {
+        private const float FullPersent = 100;
+
         private readonly int IsRuningKey = Animator.StringToHash("IsRun");
-        
+        private readonly int IsDeadKey = Animator.StringToHash("IsDead");
+        private readonly int IsTakeDamageKey = Animator.StringToHash("TakeDamage");
+
         [SerializeField] private Character _character;
         [SerializeField] private Animator _animator;
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private float _criticalPersent = 30;
+
+        private float _currentPersent;
+        private int _injeredLayerIndex;
+        private float _maxWeight = 1;
+        
+        private void Awake()
+        {
+            _injeredLayerIndex = _animator.GetLayerIndex("InjeredLayer");
+        }
 
         private void Update()
         {
-            if(_character.IsMoved)
+            _currentPersent = _character.Health / (_character.MaxHealth / FullPersent);
+            _text.text = "hp : " + _character.Health.ToString() + " % : " + _currentPersent.ToString("0.0");
+            
+            if(IsCriticalPersent())
+                _animator.SetLayerWeight(_injeredLayerIndex,_maxWeight);
+            
+            if (_character.IsMoved)
                 SetAnimationRun();
             else
                 SetAnimationIdle();
         }
-
-        private void SetAnimationRun()
-            =>_animator.SetBool(IsRuningKey, true);
         
-        private void  SetAnimationIdle()
-            =>_animator.SetBool(IsRuningKey, false);
+        private bool IsCriticalPersent()
+            => _currentPersent <= _criticalPersent;
+        
+        private void SetAnimationRun()
+            => _animator.SetBool(IsRuningKey, true);
+
+        private void SetAnimationIdle()
+            => _animator.SetBool(IsRuningKey, false);
+
+        public void SetAnimationTakeDamage()
+        {
+            if (_character.IsAlive)
+                _animator.SetTrigger(IsTakeDamageKey);
+            else
+                _animator.SetTrigger(IsDeadKey);
+        }
     }
 }
