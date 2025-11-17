84ea8ac6d21812dbbf8dc096e6cb3e6b9fb2eae5
aracool82 <aracool1982@gmail.com>
2025-11-17 18:09:34 +0700

    добавил таймер и поинты для спавнов

---
+++ _Project31/Resources/Configs/TimerConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project31.Scripts
+{
+    [CreateAssetMenu(fileName = "TimerConfig", menuName = "Config/TimerConfig")]
+    public class TimerConfig : ScriptableObject
+    {
+        [field: SerializeField] public float TimeInterval { get; set; } = 5;
+    }
+}
---
+++ _Project31/Scripts/ReactiveTimer/Timer.cs
@@ -0,0 +1,67 @@
+using _Project29_30.Scripts.ReactiveGeneric_Health;
+
+namespace _Project31.Scripts
+{
+    public class Timer
+    {
+        private ReactiveVariable<float> _maxTime;
+        private ReactiveVariable<float> _currentTime;
+        
+        private ReactiveVariable<bool> _isFinished;
+        private ReactiveVariable<bool> _isStarted;
+        
+        private bool _isRunningTime;
+
+        public Timer(float maxTime,float currentTime = 0)
+        {
+            if(currentTime > maxTime)
+                currentTime = maxTime;
+            
+            _maxTime = new ReactiveVariable<float>(maxTime);
+            _currentTime = new ReactiveVariable<float>(currentTime);
+            
+            _isFinished = new ReactiveVariable<bool>(false);
+            _isStarted = new ReactiveVariable<bool>(false);
+            
+            _isRunningTime = false;
+        }
+        
+        public IReadOnlyVariable<float> MaxTime => _maxTime;
+        public IReadOnlyVariable<float> CurrentTime => _currentTime;
+        public IReadOnlyVariable<bool> IsStarted => _isStarted; 
+        public IReadOnlyVariable<bool> IsFinished => _isFinished;
+        
+        public void Update(float deltaTime)
+        {
+            if(_isRunningTime == false)
+                return;
+            
+            _currentTime.Value += deltaTime;
+            
+            if(_currentTime.Value >= _maxTime.Value)
+                Stop();
+        }
+
+        public void Stop()
+        {
+            _isRunningTime = false;
+            _isFinished.Value = true;
+        }
+        
+        public void Resume()
+            =>_isRunningTime = true;
+
+        public void Restart()
+            => Start();
+        
+        public void Start()
+        {
+            _isRunningTime = true;
+            _currentTime.Value = 0;
+            _isFinished.Value = false;
+            
+            _isStarted.Value = true;
+            _isStarted.Value = false;
+        }
+    }
+}
